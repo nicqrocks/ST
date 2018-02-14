03a761d6037e6721b674828266ce366ab3e0ea6f
Nic Q <nicq20@gmail.com>
2018-02-14 12:15:02 -0500

    Change font to mononoki

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Fira Code:pixelsize=14:antialias=true:autohint=true";
+char font[] = "Mononoki:pixelsize=16:antialias=true:autohint=true";
 int borderpx = 2;
 
 /*
